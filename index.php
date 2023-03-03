403d3d93dd1b6e0282f1fa3baa0ff70a61c5f42c
Alumne Matí <lace8guti@gmail.com>
2023-03-03 10:05:17 +0100

    afegit codi

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+                $ArrayLletres=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','ç'];
+        foreach ($ArrayLletres as $key => $value) {
+            echo $value;
+            echo " ----";
+            //echo "<br>";
+        }
+        ?>
+    </body>
+</html>
